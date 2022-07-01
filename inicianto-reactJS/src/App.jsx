de036f8acd25ba9abc72ae8aebc900d27cec4939
GuilhermeGCFS <guilhermegcfs@yahoo.com.br>
2022-06-30 23:31:42 -0300

    commit

--- inicianto-reactJS/src/App.jsx
+++ inicianto-reactJS/src/App.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Tasks from "./components/Tasks";
+
+import "./App.css";
 
 const App = () => {
-  const message = "Vamos começar!";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar React',
+      completed: true,
+    },
+    {
+      id: '2',
+      title: 'Estudar Python',
+      completed: false,
+    },
+    {
+      id: '3',
+      title: 'Estudar JavaScript',
+      completed: false,
+    },
+    {
+      id: '4',
+      title: 'Estudar TypeScript',
+      completed: false,
+    },
+    {
+      id: '5',
+      title: 'Estudar Minha Vida',
+      completed: false,
+    },
+    
+  ]);
 
-  return <h1>{message}</h1>
+  return (
+    <>
+      <div className="containerPage">
+        <div className="containerBox">
+          <div className="content">
+            <div className="sec-one">
+              <h1>Minhas Tarefas</h1>
+              <button>
+                Adicionar
+              </button>
+            </div> 
+            <div className="sec-two">
+              <Tasks tasks={tasks} />
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
